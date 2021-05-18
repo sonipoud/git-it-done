4b2403592bb2f1da639a0e31c8af4f9a542b1dd7
Sonika <sonikapoudyal5@gmail.com>
2021-05-18 19:55:55 -0400

    created a link between pages, handled errors

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,10 +1,27 @@
+var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
+var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+  
+    if (repoName) {
+      // display repo name on the page
+      repoNameEl.textContent = repoName;
+  
+      getRepoIssues(repoName);
+    } else {
+      // if no repo was given, redirect to the homepage
+      document.location.replace("./index.html");
+    }
+  };
 
 var getRepoIssues = function (repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
     fetch(apiUrl).then(function (response) {
+        //request was successful
         if (response.ok) {
             response.json().then(function (data) {
                 displayIssues(data);
@@ -15,7 +32,8 @@ var getRepoIssues = function (repo) {
                 }
             });
         } else {
-            alert("There was a problem with your request!");
+            //if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
 };
@@ -70,5 +88,5 @@ var displayWarning = function(repo) {
   limitWarningEl.appendChild(linkEl);
 };
 
-
-getRepoIssues("facebook/react");
+getRepoName();
+// getRepoIssues("facebook/react");
